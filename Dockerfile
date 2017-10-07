e2f7b08a1a9b642c9ef78cbed202665aae2782fa
Gregory Haynes <greg@greghaynes.net>
2017-10-07 23:53:44 +0000

    Create deployment for fibonetes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,3 @@
-FROM python:3
-
-
-RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl
-
-RUN chmod +x kubectl
-
-ADD fib.py fib.py
+FROM debian
+COPY ./app /app
+ENTRYPOINT /app
--- fibonetes.go
+++ fibonetes.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"flag"
-	"os"
-	"path/filepath"
-
     apiv1 "k8s.io/api/core/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+    "k8s.io/client-go/rest"
 )
 
 func main() {
-	var kubeconfig *string
-	if home := homeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
@@ -56,11 +43,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // windows
 }
